f5a1e6cff1b214104c4e5bb9a46b30b320a9a0e6
umer.nasir <umer.nasir@pchp.net>
2026-04-12 19:17:29 -0400

    fixed iam files

--- terraform/modules/iam/outputs.tf
+++ terraform/modules/iam/outputs.tf
@@ -6,6 +6,10 @@ output "eks_node_role_arn" {
   value = aws_iam_role.eks_node.arn
 }
 
+output "lb_controller_role_arn" {
+  value = aws_iam_role.lb_controller.arn
+}
+
 output "secrets_policy_arn" {
   value = aws_iam_policy.secrets_access.arn
 }
