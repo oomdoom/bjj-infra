9c76157fb64acbaa007a56ad315036ebc5467b40
umer.nasir <umer.nasir@pchp.net>
2026-04-13 16:24:14 -0400

    cloudwatch policy attachment

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -60,6 +60,11 @@ resource "aws_iam_role_policy_attachment" "eks_ecr_read_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
+resource "aws_iam_role_policy_attachment" "eks_cloudwatch_policy" {
+  role       = aws_iam_role.eks_node.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+
 # -----------------------------
 # 3. LB Controller Role
 # -----------------------------
